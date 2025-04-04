b7d296b935e41a10e9bf4834b163c1be15f7e96e
Guillaume HUSTA <guillaume.husta@gmail.com>
2025-04-04 22:27:40 +0200

    feat: Upgrade to Spring FWK 6.x

--- src/main/java/com/example/web/WebAppInitializer.java
+++ src/main/java/com/example/web/WebAppInitializer.java
@@ -1,13 +1,12 @@
 package com.example.web;
 
 import com.example.config.AppConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 public class WebAppInitializer implements WebApplicationInitializer {
 
